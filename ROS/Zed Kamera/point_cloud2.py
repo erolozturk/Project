5cd29d259af7fa6aa70fe26b7cb3a65fbc4e8063
erolozturk <31865382+erolozturk@users.noreply.github.com>
2018-12-18 17:59:12 +0300

    Add files via upload

---
+++ ROS/Zed Kamera/point_cloud2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#roslaunch openni_launch openni.launch
+#points 640*480=307200 elemanli bir listedir
+
+from __future__ import print_function
+import sys
+import rospy
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs import point_cloud2 as pc2
+
+class point_test:
+   def callback(self, point_cloud):
+      points = list(pc2.read_points(point_cloud))
+      print (points[461440])
+
+   def __init__(self):
+      #self.point_cloud_sub = rospy.Subscriber("/camera/depth/points",PointCloud2,self.callback,queue_size=2) #kinect
+      self.point_cloud_sub = rospy.Subscriber("/zed/point_cloud/cloud_registered",PointCloud2,self.callback,queue_size=2) #zed
+def main(args):
+   pt = point_test()
+   rospy.init_node('get_point_test', anonymous=True)
+   try:
+      rospy.spin()
+   except KeyboardInterrupt:
+      print("Shutting down")
+
+if __name__ == '__main__':
+   main(sys.argv)
